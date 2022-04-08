fb48fc2cf97945f1d586ea5102c8d9d486c9d852
Mohanraj Kanagasundaram <moha.maraignaya@gmail.com>
2022-04-08 20:36:05 +0530

    1.dynamic url link updated 2.Viewsets are created 3.updation on particluar field and with particular section is done

--- procedure_app/api/urls.py
+++ procedure_app/api/urls.py
@@ -1,11 +1,28 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
+from django.conf.urls import handler404
 from procedure_app.api.views import ProcedureView,SectionView,FieldView
+from procedure_app.api.views import FieldViewSet,SectionViewSet,ProcedureViewSet,ErrorView
+router = DefaultRouter()
+router.register(r'procedures', ProcedureViewSet, basename='procedures')
+
+section_router = routers.NestedSimpleRouter(router, r'procedures', lookup='procedure')
+section_router.register(r'sections', SectionViewSet, basename='sections')
+
+field_router = routers.NestedSimpleRouter(section_router, r'sections', lookup='section')
+field_router.register(r'fields', FieldViewSet, basename='fields')
 
 urlpatterns = [
-   path('',ProcedureView.as_view(),name='Procedure List'),
-   path('<int:pk>/',ProcedureView.as_view(),name='Procedure'),
-   path('section/',SectionView.as_view(),name='section'),
-   path('section/<int:pk>/',SectionView.as_view(),name='section'),
-   path('section/field/',FieldView.as_view(),name='field'),
-   path('section/field/<int:pk>/',FieldView.as_view(),name='field')
+   path(r'', include(router.urls)),
+   path(r'', include(section_router.urls)),
+   path(r'', include(field_router.urls)),
+   # path('',ProcedureView.as_view(),name='Procedure List'),
+   # path('<int:pk>/',ProcedureView.as_view(),name='Procedure'),
+   # path('section/',SectionView.as_view(),name='section'),
+   # path('section/<int:pk>/',SectionView.as_view(),name='section'),
+   # path('section/field/',FieldView.as_view(),name='field'),
+   # path('section/field/<int:pk>/',FieldView.as_view(),name='field'),
+   # path(r'<str:pk>',ErrorView,name="page not found error")
 ]
+
